2870fa637b027b314a8199829504e85caa61a45d
Tsunami014 <tsunami014@duck.com>
2026-01-03 10:44:45 +1100

    Added a player image!

--- game/main.py
+++ game/main.py
@@ -28,11 +28,17 @@ MainSur = pygame.Surface((scale, int(scale/WIN.get_width() * WIN.get_height())))
 pygame.display.toggle_fullscreen()
 c = pygame.time.Clock()
 
+player = pygame.image.load("assets/player.png")
+playerImgs = [player.subsurface(16*i, 0, 16, 16) for i in range(player.get_width()//16)]
+playerAnim = 0
+playerAnimSpeed = 8
+
 Tileset = pygame.image.load("assets/tileset.png")
 BG_TILE = Tileset.subsurface((0, 0), (32, 32))
 
 realx, realy = 0, 0
 x, y = 0, 0
+dir = True
 
 run = True
 while run:
@@ -51,12 +57,17 @@ while run:
         dx -= 1
     if inps[pygame.K_RIGHT]:
         dx += 1
-    if dy != 0:
-        realy -= dy
-        y = math.floor(realy)
-    if dx != 0:
-        realx -= dx
-        x = math.floor(realx)
+    if dx != 0 or dy != 0:
+        playerAnim = (playerAnim + 1) % (3*playerAnimSpeed)
+        if dy != 0:
+            realy -= dy
+            y = math.floor(realy)
+        if dx != 0:
+            realx -= dx
+            x = math.floor(realx)
+            dir = dx > 0
+    else:
+        playerAnim = 0
 
     for x1 in range(x%32-32, x+32, 32):
         for y1 in range(y%32-32, MainSur.get_height()+32, 32):
@@ -71,6 +82,8 @@ while run:
         for x1 in range(x%32-32, MAP.get_width()+32*2, 32):
             MainSur.blit(BG_TILE, (x1, y1))
     MainSur.blit(MAP, (x, y))
+    MainSur.blit(pygame.transform.flip(playerImgs[math.floor(playerAnim/playerAnimSpeed)], dir, False),
+                 (MainSur.get_width()//2-8, MainSur.get_height()//2-8))
     WIN.blit(pygame.transform.scale(MainSur, WIN.get_size()),
              (realx%1 * (WIN.get_width()/scale) * 16, realy%1 * (WIN.get_height()/MainSur.get_height()) * 16))
     pygame.display.update()
--- journal.md
+++ journal.md
@@ -3,6 +3,9 @@
 - Worked on my game. Drew lots of nice tiles and made a test map! Have no code yet tho.
 - I've finished probably about half the tiles, and not including the decor. I'll finish the tiles later and work on the decor now so I can start coding soon.
 - My time tracking tool says I have spent 4 hours 53 min and 28 sec so far making assets (this doesn't include making the level, which I have no tracking method for). I hope this counts...
-# 2/1/2025
+# 2/1/25
 - Made the ldtk map auto export for usage soon
 - Added a `main.py` with almost nothing in it
+# 3/1/25
+- Added the start to the item data
+- Added the basic camera stuff
