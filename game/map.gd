4bd83ae2aa15b7a9fd3aec6b25fee715b111ef1a
Tsunami014 <tsunami014@duck.com>
2026-01-03 15:01:31 +1100

    Added player movement and collisions!

---
+++ game/map.gd
@@ -0,0 +1,47 @@
+extends CollisionPolygon2D
+
+@export_file_path("*.csv") var csv_path
+
+func _ready() -> void:
+	var file = FileAccess.open(csv_path, FileAccess.READ)
+	var toMerge = []
+
+	var y = 0
+	while !file.eof_reached():
+		var line = file.get_csv_line()
+		if line.size() >= 2:
+			for x in line.size():
+				if line[x] == "0":
+					toMerge.append(get_tile_poly(x, y))
+		y += 1
+	
+	var outPoly := PackedVector2Array(toMerge[0])
+	var attempts := 100
+	while !toMerge.is_empty() and (attempts > 0):
+		outPoly = merge_poly_array_to(outPoly, toMerge)
+		attempts -= 1
+	if !toMerge.is_empty():
+		printerr("Failed to merge in all the tiles!")
+
+	polygon = outPoly
+
+func merge_poly_array_to(merged : PackedVector2Array, toMerge: Array) -> PackedVector2Array:
+	for tilePoly in toMerge:
+		var merge_results = Geometry2D.merge_polygons(merged, tilePoly)
+		if merge_results.size() == 1:
+			merged = merge_results[0]
+			toMerge.erase(tilePoly)
+	return merged
+
+func get_tile_poly(x, y) -> PackedVector2Array:
+	var offset = Vector2(8, 8)
+	var tl = Vector2(x,   y)   * 16 - offset
+	var tr = Vector2(x+1, y)   * 16 - offset
+	var br = Vector2(x+1, y+1) * 16 - offset
+	var bl = Vector2(x,   y+1) * 16 - offset
+
+	return PackedVector2Array([tl, tr, br, bl])
+
+
+func _process(_delta: float) -> void:
+	pass
---
+++ game/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed = 40 # pixels/sec
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(_delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	move_and_slide()
+
+	if velocity.x != 0 or velocity.y != 0:
+		$AnimatedSprite2D.animation = "Walk"
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+	else:
+		$AnimatedSprite2D.animation = "Idle"
