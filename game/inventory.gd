fcb6a29c0be9eb225c4d8e7120ecedf291e74e3f
Tsunami014 <tsunami014@duck.com>
2026-01-08 10:56:16 +1100

    Added a selector square to the inventory that doesn't do much yet

--- game/inventory.gd
+++ game/inventory.gd
@@ -1,6 +1,10 @@
 extends Panel
 var inv = []
 var max_wid: int
+var cursPos: int = 0
+var lr_hold_time: int = 0
+var ud_hold_time: int = 0
+const maxHold = 20
 
 func _ready() -> void:
 	hide()
@@ -9,7 +13,41 @@ func _ready() -> void:
 	max_wid = int($Squares.size.x/20)
 
 func _tick() -> void:
-	pass
+	if len(inv) == 0:
+		$Squares/Selector.hide()
+		return
+	$Squares/Selector.show()
+	var lr = sign(Input.get_axis("move_left", "move_right"))
+	if lr != 0:
+		if lr_hold_time >= maxHold:
+			cursPos += lr
+		else:
+			if lr_hold_time == 0:
+				cursPos += lr
+			lr_hold_time += 1
+	else:
+		lr_hold_time = 0
+	var ud = sign(Input.get_axis("move_up", "move_down"))
+	if ud != 0:
+		if ud_hold_time >= maxHold:
+			cursPos += ud * max_wid
+		else:
+			if ud_hold_time == 0:
+				cursPos += ud * max_wid
+			ud_hold_time += 1
+	else:
+		ud_hold_time = 0
+
+	if cursPos <= 0:
+		cursPos = 0
+	elif cursPos >= len(inv):
+		cursPos = len(inv) - 1
+
+	$Squares/Selector.set_position(Vector2(
+		(cursPos % max_wid) * 20,
+		int(cursPos / max_wid) * 20
+	))
+
 
 func _update() -> void:
 	var myinvln = len(inv)
